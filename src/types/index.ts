a71fcdf3e3c28f4d673d9c2daad3144ad7bdd16a
golu Kumar <golukumarjaisidih@gmail.com>
2025-02-24 22:55:35 +0530

    created home page and corrected imports

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export interface Video{
+    id: string,
+    public_id: string,
+    title: string,
+    description: string,
+    publicId : string,
+    originalSize: string,
+    compressedSize: string,
+    duration:number,
+    createdAt: string
+}
